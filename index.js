9c9007e14681b135d65fe4dc0e654b939d1c8ca7
Rich Lobo <lobo.r0018@gmail.com>
2023-05-04 09:09:47 +0800

    Scalar types

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const { ApolloServer, gql } = require("apollo-server");
 const typeDefs = gql`
   type Query {
       hello: String
+      numberOfAnimals: Int
+      price: Float
+      isCool: Boolean
   }
 `
 
@@ -10,6 +13,15 @@ const resolvers = {
   Query: {
     hello: () => {
         return "World!";
+    },
+    numberOfAnimals: () => {
+      return 55;
+    },
+    price: () => {
+      return 23.09
+    },
+    isCool: () => {
+      return false
     }
   }
 }
