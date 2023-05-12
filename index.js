3b8edbed7d55adb596f907b84c67e8bb3cd4b840
Rich Lobo <lobo.r0018@gmail.com>
2023-05-12 09:03:09 +0800

    Relating data(Many to one)

--- index.js
+++ index.js
@@ -129,6 +129,7 @@ const typeDefs = gql`
     quantity: Int!
     price: Float!
     onSale: Boolean!
+    category: Category
   }
 
   type Category {
@@ -172,6 +173,12 @@ const resolvers = {
     products: (parent, args, context) => {
       const categoryId = parent.id;
       return products.filter((product) => product.categoryId === categoryId);
+    }
+  },
+  Product: {
+    category: (parent, args, context) => {
+      const categoryId = parent.categoryId;
+      return categories.find((category) => category.id === categoryId)
     }
   }
 }
