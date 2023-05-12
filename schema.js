d98d580a09215391ecd8954c01a0d09e52819873
Rich Lobo <lobo.r0018@gmail.com>
2023-05-12 09:19:33 +0800

    Reorganizing file structure for db and schema

---
+++ schema.js
@@ -0,0 +1,32 @@
+const { gql } = require("apollo-server");
+
+exports.typeDefs = gql`
+  type Query {
+    hello: String
+    # numberOfAnimals: Int
+    # price: Float
+    # isCool: Boolean
+    # list: [String]
+    products: [Product!]!
+    product(id: ID!): Product
+    categories: [Category!]!
+    category(id: ID!): Category
+  }
+
+  type Product {
+    id: ID!
+    name: String!
+    description: String!
+    image: String!
+    quantity: Int!
+    price: Float!
+    onSale: Boolean!
+    category: Category
+  }
+
+  type Category {
+    id: ID!
+    name: String!
+    products: [Product!]!
+  }
+`
