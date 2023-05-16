29ad34e1c812b934641d8a18edafd7f11d429486
Rich Lobo <lobo.r0018@gmail.com>
2023-05-16 12:39:36 +0800

    Deleting data with Set Null

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -78,5 +78,10 @@ exports.Mutation = {
     db.products = db.products.filter(product => product.id !== id);
     db.reviews = db.reviews.filter(review => review.productId !== id)
     return true
+  },
+
+  deleteReview: (parent, {id}, {db}) => {
+    db.reviews = db.reviews.filter(review => review.id !== id)
+    return true
   }
 }
