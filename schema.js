fa2c7e608fea810ee8287a618ccdf18ab49c7a87
Rich Lobo <lobo.r0018@gmail.com>
2023-05-16 10:36:05 +0800

    Creates mutation for adding product reviews

--- schema.js
+++ schema.js
@@ -16,6 +16,7 @@ exports.typeDefs = gql`
   type Mutation {
     addCategory(input: AddCategoryInput!): Category!
     addProduct(input: AddProductInput!): Product!
+    addReview(input: AddReviewInput!): Review!
   }
 
   type Product {
@@ -61,5 +62,13 @@ exports.typeDefs = gql`
     price: Float!
     onSale: Boolean!
     categoryId: String!
+  }
+
+  input AddReviewInput {
+    date: String!
+    title: String!
+    comment: String!
+    rating: Int!
+    productId: ID!
   }
 `
